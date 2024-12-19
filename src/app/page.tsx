d84f0fe1a90431fe299a19993b019097fc7e466a
georgefjtaylorwarp <georgefjtaylor@gmail.com>
2024-12-19 20:32:01 +0000

    dep fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-black tracking-tighter">Hello Vincent</h1>
+      <h1 className="text-3xl font-black tracking-tighter">LANDING PAGE</h1>
     </div>
   );
 }
-
