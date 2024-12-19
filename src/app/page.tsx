ebb4eec928107472c3b19dea64fdaab69619ee0c
georgefjtaylorwarp <georgefjtaylor@gmail.com>
2024-12-19 20:25:32 +0000

    big shaddzz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-black tracking-tighter">Hello Vincent</h1>
     </div>
   );
 }
