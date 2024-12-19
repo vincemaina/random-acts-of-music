6615aefdc4bbd15e20f88df15f8227e6578a8f3b
Vince Maina <vincemaina@proton.me>
2024-12-19 22:56:29 +0000

    display online users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/landing/header";
 import SpotifySync from "@/components/landing/spotify-sync";
+import { OnlineUsers } from "@/components/online-users";
 
 export default function Home() {
   return (
     <div className="p-4 space-y-4">
       <Header />
       <SpotifySync />
+      <OnlineUsers />
     </div>
   );
 }
