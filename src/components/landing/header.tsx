46a278fda5d585427169389777accb6adbf179e0
Vince Maina <vincemaina@proton.me>
2024-12-20 01:24:37 +0000

    center header

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <div className="flex justify-between items-center">
-            <h1 className="text-3xl font-black tracking-tighter">RANDOM ACTS OF MUSIC</h1>
+        <div className="flex justify-center items-center">
+            <h1 className="text-4xl my-3 font-black tracking-tighter">RANDOM ACTS OF MUSIC</h1>
         </div>
     )
 }
