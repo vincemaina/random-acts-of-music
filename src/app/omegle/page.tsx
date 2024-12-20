4afeb19665897abd4cb69d970a90c71e851e8b9a
Vince Maina <vincemaina@proton.me>
2024-12-20 00:28:01 +0000

    add test page

--- src/app/omegle/page.tsx
+++ src/app/omegle/page.tsx
@@ -1,51 +1,44 @@
 "use client";
 
+import Chat from "@/components/chat/chat";
 import { useEffect, useRef, useState } from "react";
-import {io} from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
 export default function Page() {
 
-    const [users, setUsers] = useState<string[]>([]);
-    const [userId, setUserId] = useState<string>(); 
-    const [recipientId, setRecipientId] = useState<string | null>(null);
+    const [chatRoom, setChatRoom] = useState(null);
+    const [matchedUser, setMatchedUser] = useState<string>("");
+    const [socket, setSocket] = useState<Socket>();
 
     useEffect(() => {
         const socket = io("http://localhost:3001");
 
-        socket.on('connect', () => {
-            console.log("Connected:", socket.id);
-            setUserId(socket.id);
+        // When the user is matched, receive the chat room and the other user
+        socket.on('matched', (data) => {
+            console.log("data:", data);
+            setChatRoom(data.chatRoom);
+            setMatchedUser(data.userId);
+            console.log(`Matched with user ${data.userId} in room ${data.chatRoom}`);
         });
 
-        socket.on('online-users', (users) => {
-            console.log("New user online:", users);
-            setUsers(users);
-        });
+        setSocket(socket);
 
         // Cleanup listener on unmount
         return () => {
-            socket.off('online-users');
+            socket.off('matched');
         };
     }, []);
 
-    useEffect(() => {
-        if (users.length > 1 && userId) {
-            const otherUsers = users.filter(user => user != userId);
-            const randomIndex = Math.floor(Math.random() * otherUsers.length);
-            const randomUser = otherUsers[randomIndex];
-            console.log("Chatting with random user:", randomUser);
-            setRecipientId(randomUser);
-        }
-    }, [users, userId]);
+    if (!socket) return <>Connecting...</>
 
-    if (!userId) return <>Connecting...</>
-
-    if (users.length <= 1) return <>Waiting for other users to join...</>
+    if (!chatRoom) return <>Waiting for friends...</>
 
     return (
         <>
-            <div>You are: {userId}</div>
-            <div>Chatting with: {recipientId}</div>
+            <div>Chat room name: {chatRoom}</div>
+            <div>Chatting with: {matchedUser}</div>
+
+            <Chat chatRoom={chatRoom} recipientId={matchedUser} socket={socket}/>
         </>
     )
 }
