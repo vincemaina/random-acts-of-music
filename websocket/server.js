e63739bd24e38dc1754e336c1c9a97ed6c6c58a6
Vince Maina <vincemaina@proton.me>
2024-12-20 00:27:47 +0000

    create matches on server

--- websocket/server.js
+++ websocket/server.js
@@ -8,26 +8,55 @@ const io = new Server(httpServer, {
   }
 });
 
-let onlineUsers = [];
+let waitingRoom = [];  // Users waiting to be matched
 
 io.on("connection", (socket) => {
-  console.log("New connection:", socket.id);
 
-  // Add user to the "online" list with their socket ID
-  onlineUsers.push(socket.id);
+  // Add user to the "waiting room" list with their socket ID
+  waitingRoom.push(socket.id);
+  console.log(`User ${socket.id} added to the waiting room`);
 
-  // Emit the updated list of online users to everyone
-  io.emit('online-users', onlineUsers);
+  // // Emit the updated list of online users to everyone
+  // io.emit('waiting-room', waitingRoom);
+
+  // Check if we can match users
+  if (waitingRoom.length >= 2) {
+    // Get two users to match
+    const user1 = waitingRoom.shift(); // First user in the waiting room
+    const user2 = waitingRoom.shift(); // Second user in the waiting room
+
+    // Create a unique chat room
+    const chatRoom = `${user1}-${user2}`;
+
+    // Assign users to the private chat room
+    io.to(user1).emit('matched', { userId: user2, chatRoom });
+    io.to(user2).emit('matched', { userId: user1, chatRoom });
+
+    // Join both users to the chat room
+    io.sockets.sockets.get(user1).join(chatRoom);
+    io.sockets.sockets.get(user2).join(chatRoom);
+
+    console.log(`Matched users ${user1} and ${user2} in room ${chatRoom}`);
+  }
+
+  socket.on('new-message', (data) => {
+    const { chatRoom, message } = data;
+
+    // Emit the message to the other user in the chat room
+    socket.to(chatRoom).emit('new-message', message);
+
+    console.log(`New message in room ${chatRoom}: ${message.content}`);
+  });
 
   // Handle user disconnect
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
 
     // Remove the user from the online users list
-    onlineUsers = onlineUsers.filter((user) => user !== socket.id);
+    waitingRoom = waitingRoom.filter((user) => user !== socket.id);
 
     // Emit the updated list of online users to everyone
-    io.emit('online-users', onlineUsers);
+    io.emit('online-users', waitingRoom.length);
   });
 });
 
