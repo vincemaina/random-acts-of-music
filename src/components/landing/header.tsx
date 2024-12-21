5c2573f3dddb4b2dc759cb4ef0b1fde3e61ee819
Vince Maina <vincemaina@proton.me>
2024-12-21 20:09:26 +0000

    add link back to homepage

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
         <div className="relative sm:min-h-0 mb-2">
             <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center">
                 <h1 className="text-2xl sm:text-4xl font-black tracking-tighter">
-                    RANDOM ACTS OF <span className="text-[#752add]">MUSIC</span>
+                    <Link href={"/"}>
+                        RANDOM ACTS OF <span className="text-[#752add]">MUSIC</span>
+                    </Link>
                 </h1>
             </div>
         </div>
