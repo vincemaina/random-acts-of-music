7559882ad4e173d288c6cb01094d944b7b4c878f
Vince Maina <vincemaina@proton.me>
2024-12-22 21:57:24 +0000

    a feeble attempt to fix iframe + fix colours

--- src/components/chat/public-chat.tsx
+++ src/components/chat/public-chat.tsx
@@ -194,27 +194,27 @@ export default function PublicChat({ socket }: Props) {
                                     {!isSameUserAsNext && (
                                         <p
                                             className={`text-xs font-semibold italic mx-1 mb-1.5 ${index !== messages.length - 1 ? "mt-10" : ""} ${message.userName === getUsername()
-                                                    ? "text-right text-gray-500"
-                                                    : "text-left text-gray-700"
+                                                ? "text-right text-gray-500"
+                                                : "text-left text-gray-700"
                                                 }`}
-                                            style={{color: hashStringToColor(message.userName!)}}
+                                            style={{ color: hashStringToColor(message.userName!) }}
                                         >
                                             {message.userName}
                                         </p>
                                     )}
                                     <div
                                         className={`flex text-sm ${message.userName === getUsername()
-                                                ? "justify-end"
-                                                : "justify-start"
+                                            ? "justify-end"
+                                            : "justify-start"
                                             }`}
                                     >
                                         <div
-                                            className={`max-w-[85%] sm:max-w-[70%] rounded-lg shadow ${message.isTrack
-                                                    ? "p-0 m-0 overflow-hidden"
-                                                    : `${message.userName === getUsername()
-                                                        ? "bg-neutral-900 text-white"
-                                                        : "bg-white text-black"
-                                                    } py-1.5 px-2.5`
+                                            className={`max-w-[85%] sm:max-w-[40%] rounded-lg ${message.isTrack
+                                                ? "p-0 m-0 overflow-hidden"
+                                                : `shadow ${message.userName === getUsername()
+                                                    ? "bg-neutral-900 text-white"
+                                                    : "bg-white text-black"
+                                                } py-1.5 px-2.5`
                                                 }`}
                                             style={{
                                                 // color: message.userName !== getUsername() ? hashStringToColor(message.userName!) : undefined,
@@ -222,17 +222,9 @@ export default function PublicChat({ socket }: Props) {
                                             }}
                                         >
                                             {message.isTrack ? (
-                                                <iframe
-                                                    style={{ borderRadius: "12px" }}
-                                                    src={`https://open.spotify.com/embed/track/${extractSpotifyTrackId(
-                                                        message.trackData?.spotifyUrl || ""
-                                                    )}?utm_source=generator`}
-                                                    width="100%"
-                                                    height="100%"
-                                                    allowFullScreen
-                                                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                                                    loading="lazy"
-                                                />
+                                                <iframe style={{borderRadius: "12px", height: 80, paddingBottom: 0}} src={`https://open.spotify.com/embed/track/${extractSpotifyTrackId(
+                                                    message.trackData?.spotifyUrl || ""
+                                                )}?utm_source=generator`} width="100%" height="100%" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                                             ) : (
                                                 <p>{message.content}</p>
                                             )}
