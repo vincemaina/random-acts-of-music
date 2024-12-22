a6246b4bad5b5594483ab060e1ec04554368c9d0
Vince Maina <vincemaina@proton.me>
2024-12-22 00:00:45 +0000

    changes to publics acts of music on public pages

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+
+    const path = usePathname();
+
     return (
         <div className="relative sm:min-h-0 mb-2">
             <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center">
                 <h1 className="text-2xl sm:text-4xl font-black tracking-tighter">
                     <Link href={"/"}>
-                        RANDOM ACTS OF <span className="text-[#752add]">MUSIC</span>
+                        {path.startsWith('/public') ? "PUBLIC" : "RANDOM"} ACTS OF <span className="text-[#752add]">MUSIC</span>
                     </Link>
                 </h1>
             </div>
